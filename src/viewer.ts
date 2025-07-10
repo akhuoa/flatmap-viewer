40b18e18b4cca201d36501dab9fe963d8c35b05a
David Brooks <d.brooks@auckland.ac.nz>
2025-07-11 10:00:44 +1200

    Release as version `4.2.9`.

--- src/viewer.ts
+++ src/viewer.ts
@@ -36,7 +36,7 @@ import * as utils from './utils'
 //==============================================================================
 
 // The released version of the viewer
-export const VIEWER_VERSION = '4.2.8'
+export const VIEWER_VERSION = '4.2.9'
 
 //==============================================================================
 
