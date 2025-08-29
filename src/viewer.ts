20315a64126a3317fdac725902d3e1a18e22e4a7
David Brooks <d.brooks@auckland.ac.nz>
2025-08-29 12:42:10 +1200

    Release as version `4.2.14`

--- src/viewer.ts
+++ src/viewer.ts
@@ -34,7 +34,7 @@ import * as utils from './utils'
 //==============================================================================
 
 // The released version of the viewer
-export const VIEWER_VERSION = '4.2.13'
+export const VIEWER_VERSION = '4.2.14'
 
 //==============================================================================
 
